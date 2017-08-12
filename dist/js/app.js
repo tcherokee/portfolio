2db76ecc503a1ef019af6dcccde532dd02f0ed62
Tolumi Adamson <tolumi.adamson@kenshomedia.com>
2017-08-12 19:38:03 +0100

    Added Typewriter Effect

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,11 +1,66 @@
 'use strict';
 
-var arrow = document.getElementsByClassName('arrow');
+var index = 0;
+var arrow = document.getElementById('scrollArrow');
+var whoAmISpan = document.getElementById('whoAmI');
+var whoAmIText = ["Web Designer", "Web Developer", "Husband", "Father", "Chelsea Fan", "Internet Busybody"];
+var originalPosition = arrow.getBoundingClientRect().top;
 
-var testArrow = arrow[0];
-
-function fadeArrow(arrow) {
-  var arrowOpacity = (arrow.getBoundingClientRect().top - window.scrollY) / arrow.getBoundingClientRect().top;
-  console.log(arrowOpacity);
+function fadeArrow() {
+  var opacity = arrow.getBoundingClientRect().top / originalPosition;
+  arrow.style.opacity = opacity;
+  console.log("t");
 }
+
+function whoAmI() {
+  var arrItem = whoAmIText[index];
+
+  if (index < whoAmIText.length) {
+    whoAmISpan.innerHTML = "";
+    typewriterEffect(arrItem, 0);
+  } else {
+    setTimeout(function () {
+      index = 0;
+      whoAmI();
+    }, 2000);
+  }
+}
+
+function typewriterEffect(arr, i) {
+  var typeDelay = 200;
+
+  if (i < arr.length) {
+    setTimeout(function () {
+      whoAmISpan.append(arr.charAt(i));
+      i += 1;
+      typewriterEffect(arr, i);
+    }, typeDelay);
+  } else {
+    setTimeout(function () {
+      typewriterBackspace(whoAmISpan.innerHTML, 0);
+    }, 1000);
+  }
+}
+
+function typewriterBackspace(arr, i) {
+  var typeDelay = 50;
+  var slicedArr = void 0;
+
+  if (i < arr.length) {
+    setTimeout(function () {
+      slicedArr = whoAmISpan.innerHTML.slice(0, -1);
+      whoAmISpan.innerHTML = slicedArr;
+      typewriterBackspace(slicedArr, i);
+    }, typeDelay);
+  } else {
+    setTimeout(function () {
+      index += 1;
+      whoAmI();
+    }, 500);
+  }
+}
+
+window.addEventListener("scroll", fadeArrow);
+
+whoAmI();
 //# sourceMappingURL=app.js.map
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,72 @@
-var arrow = document.getElementsByClassName('arrow');
+let index = 0;
+let arrow = document.getElementById('scrollArrow');
+let whoAmISpan = document.getElementById('whoAmI');
+let whoAmIText =  [
+                    "Web Designer",
+                    "Web Developer",
+                    "Husband",
+                    "Father",
+                    "Chelsea Fan",
+                    "Internet Busybody"
+                  ];
+const originalPosition = arrow.getBoundingClientRect().top;
 
-var testArrow = arrow[0];
 
-function fadeArrow(arrow) {
-  const originalPosition = arrow.getBoundingClientRect().top;
-  let opacity = (arrow.getBoundingClientRect().top - window.scrollY)/arrow.getBoundingClientRect().top
-  console.log(arrowOpacity);
+function fadeArrow() {
+  let opacity = arrow.getBoundingClientRect().top/originalPosition;
+  arrow.style.opacity = opacity;
+  console.log("t");
 }
+
+function whoAmI() {
+  let arrItem = whoAmIText[index];
+
+  if (index < whoAmIText.length) {
+    whoAmISpan.innerHTML = "";
+    typewriterEffect(arrItem, 0);
+
+  } else {
+    setTimeout(function() {
+      index = 0;
+      whoAmI();
+    }, 2000);
+  }
+}
+
+function typewriterEffect(arr, i) {
+  let typeDelay = 200;
+
+  if (i < arr.length) {
+    setTimeout(function(){
+      whoAmISpan.append(arr.charAt(i));
+      i += 1;
+      typewriterEffect(arr, i);
+    }, typeDelay);
+  } else {
+    setTimeout(function(){
+      typewriterBackspace(whoAmISpan.innerHTML, 0)
+    }, 1000)
+  }
+}
+
+function typewriterBackspace(arr, i) {
+  let typeDelay = 50;
+  let slicedArr;
+
+  if (i < arr.length) {
+    setTimeout(function(){
+      slicedArr = whoAmISpan.innerHTML.slice(0, -1);
+      whoAmISpan.innerHTML = slicedArr
+      typewriterBackspace(slicedArr, i);
+    }, typeDelay);
+  } else {
+    setTimeout(function(){
+      index += 1;
+      whoAmI();
+    }, 500)
+  }
+}
+
+window.addEventListener("scroll", fadeArrow);
+
+whoAmI();
